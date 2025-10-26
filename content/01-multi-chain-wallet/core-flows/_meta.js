fb9733bf1db3504be26536bab6a62baf5c181fc3
gary <lujialei08@gmail.com>
2025-10-27 06:44:45 +0800

    update

--- content/01-multi-chain-wallet/core-flows/_meta.js
+++ content/01-multi-chain-wallet/core-flows/_meta.js
@@ -1,3 +1,6 @@
 export default {
-  '01-create-wallet': '1. 创建钱包'
+  '01-create-wallet': '1. 创建钱包',
+  '02-import-wallet': '2. 导入钱包',
+  '03-create-account': '3. 创建账户',
+  '04-query-accounts': '4. 查询账户'
 }
